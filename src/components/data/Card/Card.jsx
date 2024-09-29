2a30c68566176bdc7e94b8b42a153bb4f3a2762c
TatyanaPanchenko <tatyanapanchenko.web@gmail.com>
2024-09-29 13:53:04 +0300

    Change project

--- src/components/data/Card/Card.jsx
+++ src/components/data/Card/Card.jsx
@@ -1,12 +1,14 @@
 import style from "./card.module.scss";
+import createUniqueID from "../../../createUniqueID";
+
 export default function Card(props) {
   return (
     <div className={style["card-wrapper"]}>
-      {props.data.map((item, index) => {
+      {props.data.map((item) => {
         return (
-          <div className={style["card"]} key={index}>
-            <div className={style["card-inner"]} key={index}>
-              <h2 className={style["card-country"]} key={index}>
+          <div className={style["card"]} key={createUniqueID()}>
+            <div className={style["card-inner"]} key={createUniqueID()}>
+              <h2 className={style["card-country"]} key={createUniqueID()}>
                 {item.name}
               </h2>
               <div className={style["country-continent"]}>{item.continent}</div>
@@ -15,22 +17,40 @@ export default function Card(props) {
               </div>
               {item.tournament.map((item, index) => {
                 return (
-                  <div className={style["card-matches"]} key={index}>
+                  <div className={style["card-matches"]} key={createUniqueID()}>
                     <div className={style["match-name"]}>{item.name}</div>
-                    <div className={style["card-events"]} key={index}>
+                    <div
+                      className={style["card-events"]}
+                      key={createUniqueID()}
+                    >
                       {item.event.map((item, index) => {
                         return (
-                          <div className={style["card-event"]} key={index}>
-                            <div className={style["event-name"]} key={index}>
+                          <div
+                            className={style["card-event"]}
+                            key={createUniqueID()}
+                          >
+                            <div
+                              className={style["event-name"]}
+                              key={createUniqueID()}
+                            >
                               {item.player}
                             </div>
-                            <div className={style["event-minute"]} key={index}>
+                            <div
+                              className={style["event-minute"]}
+                              key={createUniqueID()}
+                            >
                               {item.minute} minutes
                             </div>
-                            <div className={style["event-team"]} key={index}>
+                            <div
+                              className={style["event-team"]}
+                              key={createUniqueID()}
+                            >
                               Team: {item.team}
                             </div>
-                            <div className={style["event-type"]} key={index}>
+                            <div
+                              className={style["event-type"]}
+                              key={createUniqueID()}
+                            >
                               Type: {item.type}
                             </div>
                           </div>
